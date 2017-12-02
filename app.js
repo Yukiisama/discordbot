f704c1a37d8858363a10b295f5ccaec5d6134fe7
Alvin H <alvinhan1999@gmail.com>
2017-12-02 16:38:43 -0500

    Merge branch 'master' of https://github.com/edavidj/discordbot

--- app.js
+++ app.js
@@ -39,12 +39,38 @@ client.on('message', message => {
     } else if (params[0] === "!mastery") {
         let url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+params[1]+'?api_key=RGAPI-fb155021-23cc-4070-98b2-d36f1837e462';
         Request(message, url, (data) => {
+          try{
             var championID = getChampID(params[2]);
-            let url = 'https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/' + data.id + '/by-champion/' + championID + `?api_key=${config.api_key}`;
+            let url = `https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/${data.id}/by-champion/${championID}?api_key=${config.api_key}`;
             Request(message, url, (stuff) => {
                 console.log(stuff);
                 message.reply(stuff.championLevel);
             });
+          } catch(err) {
+            message.channel.send('Usage: !mastery <summoner> <champion>');
+          }
+        });
+    } else if (params[0] === "!haschest") {
+        let url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+params[1]+'?api_key=RGAPI-fb155021-23cc-4070-98b2-d36f1837e462';
+        Request(message, url, (data) => {
+            try {
+              var championID = getChampID(params[2]);
+              let url = `https://na1.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/${data.id}/by-champion/${championID}?api_key=${config.api_key}`;
+              Request(message, url, (stuff) => {
+                  console.log(stuff);
+                  if (stuff.status == undefined) {
+                    if (stuff.chestGranted == true) {
+                      message.reply("you have received the chest for " + params[2]);
+                    } else if (stuff.chestGranted == false) {
+                      message.reply("you have not received the chest for " + params[2]);
+                    }
+                  } else if (stuff.status != undefined) {
+                      message.channel.send("Riot API Error: " + errorMessages(stuff.status.status_code))
+                  }
+              });
+            } catch(err) {
+              message.channel.send('Usage: !haschest <summoner> <champion>');
+            }
         });
     }
 
@@ -82,6 +108,43 @@ function getQueueIndex(q, arr){
 }
 function getChampID(input){
   return champions.champList.data[input].id;
+}
+function errorMessages(errorCode) {
+  switch (errorCode) {
+    case 400:
+      return "Bad request";
+      break;
+    case 401:
+      return "Unauthorized";
+      break;
+    case 403:
+      return "Forbidden";
+      break;
+    case 404:
+      return "Data not found";
+      break;
+    case 405:
+      return "Method not allowed";
+      break;
+    case 415:
+      return "Unsupported media type";
+      break;
+    case 429:
+      return "Rate limit exceeded";
+      break;
+    case 500:
+      return "Internal service error";
+      break;
+    case 502:
+      return "Bad gateway";
+      break;
+    case 503:
+      return "Service unavailable";
+      break;
+    case 504:
+      return "Gateway timeeout";
+      break;
+  }
 }
 
 client.login(config.token);
