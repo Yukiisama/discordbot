5b07984a7105372c6f7d566f3364797f48f0b9bc
jennifer-c <34190173+jennifer-c@users.noreply.github.com>
2017-12-02 15:48:19 -0500

    testing icons

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const champions = require("./champions.js");
 
 client.on('ready', () => {
     console.log('I am ready!');
-<<<<<<< HEAD
   });
-=======
-});
->>>>>>> 696c37e108da732e5cd3ee42db5c8c7c2b0f0f75
 
 client.on('message', message => {
     let params = message.content.split(" ");
@@ -22,17 +18,26 @@ client.on('message', message => {
         Request(message, url, (data) => {
             message.reply(data.summonerLevel);
         });
+
     } else if(params[0] === "!rank"){
-        let url = 'https://na1.api.riotgames.com/lol/league/v3/leagues/'+params[1]+'?api_key='+config.api_key;
+        let url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+params[1]+'?api_key='+config.api_key;
         Request(message, url, (data) => {
-<<<<<<< HEAD
-          let url = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/'+data.id+'?api_key='+config.api_key;
-          Request(message, url, (rankData) => {
-              // let queue = params[2]
-              let index = getQueueIndex("RANKED_SOLO_5x5", rankData);
-              message.reply(rankData[index].tier + " "+rankData[index].rank);
-          });
+            let url = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/'+data.id+'?api_key='+config.api_key;
+            Request(message, url, (rankData) => {
+                try{
+                    var convertQueue = {
+                        "flex":"RANKED_FLEX_SR",
+                        "solo":"RANKED_SOLO_5x5"
+                    }
+                    let queue = params[2] !== undefined ? convertQueue[params[2].toLowerCase()] : "RANKED_SOLO_5x5";
+                    let index = getQueueIndex(queue, rankData);
+                    message.reply(rankData[index].tier + " "+rankData[index].rank);
+                } catch(err){
+                    message.reply("Usage: !rank <summoner> <queue>[solo, flex]");
+                }
+            });
         });
+
     } else if(params[0] === "!winrate") {
       let url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+params[1]+'?api_key='+config.api_key;
       Request(message, url, (data) => {
@@ -51,31 +56,7 @@ client.on('message', message => {
               }
           });
         });
-    } else if(params[0] === "") {
 
-    }
-
-
-});
-/**
- *
- * @param {*} message
-=======
-            let url = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/'+data.id+'?api_key='+config.api_key;
-            Request(message, url, (rankData) => {
-                try{
-                    var convertQueue = {
-                        "flex":"RANKED_FLEX_SR",
-                        "solo":"RANKED_SOLO_5x5"
-                    }
-                    let queue = params[2] !== undefined ? convertQueue[params[2].toLowerCase()] : "RANKED_SOLO_5x5";
-                    let index = getQueueIndex(queue, rankData);
-                    message.reply(rankData[index].tier + " "+rankData[index].rank);
-                } catch(err){
-                    message.reply("Usage: !rank <summoner> <queue>[solo, flex]");
-                }
-            });
-        });
     } else if (params[0] === "!mastery") {
         let url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+params[1]+'?api_key=RGAPI-fb155021-23cc-4070-98b2-d36f1837e462';
         Request(message, url, (data) => {
@@ -86,13 +67,22 @@ client.on('message', message => {
                 message.reply(stuff.championLevel);
             });
         });
+
+    } else if(params[0] === "!lastplayed") {
+      let url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+params[1]+'?api_key='+config.api_key;
+      Request(message, url, (data) => {
+        var championID = getChampID(params[2]);
+        let url = 'https://na1.api.riotgames.com/lol/static-data/v3/champions/'+championID+'?api_key='+config.api_key;
+          Request (message, url, (champData) => {
+            console.log(champData);
+            message.reply(champData.image);
     }
+
 
 });
 /**
  *
- * @param {*} message Object for reading and send messages
->>>>>>> 696c37e108da732e5cd3ee42db5c8c7c2b0f0f75
+ * @param {*} message
  * @param {*} params {name:""}
  * @param {*} callback
  */
@@ -122,14 +112,12 @@ function getQueueIndex (q, arr){
   }
   return -1;
 }
-<<<<<<< HEAD
 
-
-
-=======
 function getChampID(input){
   return champions.champList.data[input].id;
 }
->>>>>>> 696c37e108da732e5cd3ee42db5c8c7c2b0f0f75
+
+
+
 
 client.login(config.token);
